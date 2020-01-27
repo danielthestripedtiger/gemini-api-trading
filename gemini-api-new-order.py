aedf0a27d7e8f08b1b9dd669c34760aed78f4673
danielthestripedtiger <60051207+danielthestripedtiger@users.noreply.github.com>
2020-01-27 00:21:36 -0600

    added new order script

---
+++ gemini-api-new-order.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import base64
+import hmac
+import hashlib
+import datetime, time
+
+base_url = "https://api.gemini.com"
+endpoint = "/v1/order/new"
+url = base_url + endpoint
+
+gemini_api_key = "API_KEY_GOES_HERE"
+gemini_api_secret = "API_SECRET_GOES_HERE".encode()
+
+t = datetime.datetime.now()
+payload_nonce =  str(int(time.mktime(t.timetuple())*1000))
+
+payload = {
+   "request": "/v1/order/new",
+    "nonce": payload_nonce,
+    "symbol": "ethusd",
+    "amount": "0.5",
+    "price": "163.41",
+    "side": "buy",
+    "type": "exchange limit",
+    "options": ["immediate-or-cancel"] 
+}
+
+encoded_payload = json.dumps(payload).encode()
+b64 = base64.b64encode(encoded_payload)
+signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+
+request_headers = { 'Content-Type': "text/plain",
+                    'Content-Length': "0",
+                    'X-GEMINI-APIKEY': gemini_api_key,
+                    'X-GEMINI-PAYLOAD': b64,
+                    'X-GEMINI-SIGNATURE': signature,
+                    'Cache-Control': "no-cache" }
+
+response = requests.post(url,
+                        data=None,
+                        headers=request_headers)
+
+new_order = response.json()
+print(new_order)
